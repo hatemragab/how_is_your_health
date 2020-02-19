31c8f9d59e1f8c35e6b6924aaff10ed9a71432e7
Amir <amirsaber303@gmail.com>
2020-02-18 23:05:08 -0800

    add home instead of my app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:how_is_your_health/main.dart';
+import 'package:how_is_your_health/screens/Home.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(Home());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
